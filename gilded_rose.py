5b987f9f24c1dd2a587005c3279c2dd857b87407
Mengwen Yang <mwenyang2016@gmail.com>
2021-02-04 18:43:17 +0100

    all items coded for now

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,45 +1,91 @@
 # -*- coding: utf-8 -*-
 
+min_quality = 0
+max_quality = 50
+legendary_quality = 80
+min_sell_in = 0
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
+
     def update_quality(self):
         for item in self.items:
+
             if item.name == "Aged Brie":
                 if item.sell_in > 0:
                     item.sell_in -= 1
-                if item.quality <= 50:
-                    item.quality += 1
-
-            if item.name == "Conjured Mana Cake":
+                    if item.quality < 0:
+                        item.quality = 0
+                    elif item.quality < 50:
+                        item.quality += 1
+                    else:
+                         item.quality = 50
+                else:
+                    item.sell_in -= 1
+                    if item.quality < 0:
+                        item.quality = 0
+                    elif item.quality < 50:
+                        item.quality += 2
+                    else:
+                         item.quality = 50
+          
+          
+            elif item.name == "Conjured Mana Cake":
                 if item.sell_in > 0:
                     item.sell_in -= 1
-                if item.quality <= 50:
-                    item.quality -= 2
-
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                item.quality = 80
-                if item.sell_in < 0:
-                    item.sell_in = 0
-
-            if item.name == "Backstage passes to a TAFKAL80ETC concert": # deze klopt niet
-                if item.sell_in < 0:
-                    item.sell_in = 0
-                elif item.sell_in <= 5:
-                    item.quality += 3
+                    if item.quality < 0:
+                        item.quality = 0
+                    elif item.quality < 50:
+                        item.quality -= 2
+                    else:
+                         item.quality = 50
+                elif item.sell_in < 0:   # sellin expired date
                     item.sell_in -= 1
-                elif item.sell_in <= 10:
-                    item.quality += 2
-                    item.sell_in -= 1
-                else:
-                    #  item.sell_in > 10:
-                    item.quality += 1
-                    item.sell_in -= 1
-               
+                    if item.quality < 0:
+                        item.quality = 0
+                    elif item.quality > 0: 
+                         item.quality -= 4
                     
+                    
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                if item.quality < 80:
+                    item.quality = 80
+                else: 
+                    item.quality = 80
+
                 
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert": # deze klopt niet
+                if item.sell_in > 10:
+                    item.sell_in -= 1
+                    if item.quality > 50:
+                        item.quality = 50
+                    else:
+                        item.quality += 1
+               
+                elif item.sell_in > 5:
+                    item.sell_in -= 1
+                    if item.quality > 50:
+                        item.quality = 50
+                    elif item.quality >= 0:
+                        item.quality += 2
+                    else:
+                        item.quality = 0
+               
+                elif item.sell_in > 0:
+                    item.sell_in -= 1
+                    if item.quality > 50:
+                        item.quality = 50
+                    elif item.quality >= 0:
+                        item.quality += 3
+                    else:
+                        item.quality = 0
+                    
+                else:
+                    item.sell_in -=1
+                    item.quality = 0
                 
 
            
